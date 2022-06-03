992fee4359a41be6b6c8cd5adc24ef88b4612d06
Jorge <gomeztorresjorgeisaac@gmail.com>
2022-06-03 18:14:22 -0500

    Primera subida 9

--- app/Http/Controllers/Api/ImagenApiController.php
+++ app/Http/Controllers/Api/ImagenApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Images;
+use Input;
 
 class ImagenApiController extends Controller
 {
