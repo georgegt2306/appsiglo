d482fdf74378eec9c89c403cd62499c9e67bdd76
Jorge Gomez Torres <jgomez@siglo21.net>
2022-09-22 14:12:36 -0500

    Mejoras cargadas 59

--- app/Http/Controllers/Api/ProdApiController.php
+++ app/Http/Controllers/Api/ProdApiController.php
@@ -10,8 +10,7 @@ class ProdApiController extends Controller
 {
     
     public function producto(Request $request)
-    {
-        
+    {   
         $result=Producto::where('cod_producto',$request->cod_producto)
                 ->whereNull('deleted_at')
                 ->orderBy('id')
@@ -28,15 +27,27 @@ class ProdApiController extends Controller
     
     public function totalproduct(Request $request)
     {
-        
-        $result= Producto::paginate(10);
 
-        return response()->json(["sms"=>true, "data"=>$result]);
-        // if(!is_null($result)){
-        //     return response()->json(["sms"=>true, "data"=>["cod_producto"=>$result->cod_producto, "nombre"=>$result->nombre, "valor"=>$result->valor_premio]]);
-        // }else{
-        //     return response()->json(["sms"=>false,"data"=>"Codigo no existente"]);
-        // }
+        $filtros=[];
+        if(strcmp($request->marca,'')!=0){
+            array_push($filtros, ['marca','=',$request->marca]);
+        }
+        if(strcmp($request->NombreNivel1,'')!=0){
+            array_push($filtros, ['NombreNivel1','=',$request->NombreNivel1]);
+        }
+        if(strcmp($request->NombreNivel2,'')!=0){
+            array_push($filtros, ['NombreNivel2','=',$request->NombreNivel2]);
+        }
+        if(strcmp($request->NombreNivel3,'')!=0){
+            array_push($filtros, ['NombreNivel3','=',$request->NombreNivel3]);
+        }       
+        if(strcmp($request->nombre,'')!=0){
+            array_push($filtros, ['nombre','LIKE', '%'.$request->nombre.'%']);
+        }   
+
+        $result= Producto::where($filtros)->WhereNull('deleted_at')->cursorPaginate();
+
+        return response()->json(["sms"=>true, "data"=>$result ]);
        
     }
 }
